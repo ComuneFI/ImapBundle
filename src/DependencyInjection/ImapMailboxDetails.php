cf18eb3b0a5a9ed93c77cbf6ca5744528df55faa
d59495 <andrea.manzi@comune.fi.it>
2016-10-10 13:34:10 +0200

    refactoring

--- src/DependencyInjection/ImapMailboxDetails.php
+++ src/DependencyInjection/ImapMailboxDetails.php
@@ -100,4 +100,111 @@ class ImapMailboxDetails
         $attachment->contents = $attachmentdata;
         $mail->addAttachment($attachment);
     }
+
+    /**
+     * Fetch mail headers for listed mails ids.
+     *
+     * Returns an array of objects describing one mail header each. The object will only define a property if it exists. The possible properties are:
+     * subject - the mails subject
+     * from - who sent it
+     * to - recipient
+     * date - when was it sent
+     * message_id - Mail-ID
+     * references - is a reference to this mail id
+     * in_reply_to - is a reply to this mail id
+     * size - size in bytes
+     * uid - UID the mail has in the mailbox
+     * msgno - mail sequence number in the mailbox
+     * recent - this mail is flagged as recent
+     * flagged - this mail is flagged
+     * answered - this mail is flagged as answered
+     * deleted - this mail is flagged for deletion
+     * seen - this mail is flagged as already read
+     * draft - this mail is flagged as being a draft
+     *
+     * @param array $mailsIds
+     *
+     * @return array
+     */
+    public function getMailsInfo(array $mailsIds)
+    {
+        $mails = imap_fetch_overview(ImapStreamUtils::getImapStream($this->imapPath, $this->login, $this->password, constant($this->imapopenoptions)), implode(',', $mailsIds), FT_UID);
+        if (is_array($mails) && count($mails)) {
+            foreach ($mails as &$mail) {
+                self::setMaildetail($mail, $this->serverEncoding);
+            }
+        }
+
+        return $mails;
+    }
+
+    /**
+     * Get information about the current mailbox.
+     *
+     * Returns an object with following properties:
+     * Date - last change (current datetime)
+     * Driver - driver
+     * Mailbox - name of the mailbox
+     * Nmsgs - number of messages
+     * Recent - number of recent messages
+     * Unread - number of unread messages
+     * Deleted - number of deleted messages
+     * Size - mailbox size
+     *
+     * @return object Object with info | FALSE on failure
+     */
+    public function getMailboxInfo()
+    {
+        return imap_mailboxmsginfo(ImapStreamUtils::getImapStream($this->imapPath, $this->login, $this->password, constant($this->imapopenoptions)));
+    }
+
+    /**
+     * Get mails count in mail box.
+     *
+     * @return int
+     */
+    public function countMails()
+    {
+        return imap_num_msg(ImapStreamUtils::getImapStream($this->imapPath, $this->login, $this->password, constant($this->imapopenoptions)));
+    }
+
+    /**
+     * Retrieve the quota settings per user.
+     *
+     * @return array - FALSE in the case of call failure
+     */
+    protected function getQuota()
+    {
+        return imap_get_quotaroot(ImapStreamUtils::getImapStream($this->imapPath, $this->login, $this->password, constant($this->imapopenoptions)), 'INBOX');
+    }
+
+    /**
+     * Return quota limit in KB.
+     *
+     * @return int - FALSE in the case of call failure
+     */
+    public function getQuotaLimit()
+    {
+        $quota = $this->getQuota();
+        if (is_array($quota)) {
+            $quota = $quota['STORAGE']['limit'];
+        }
+
+        return $quota;
+    }
+
+    /**
+     * Return quota usage in KB.
+     *
+     * @return int - FALSE in the case of call failure
+     */
+    public function getQuotaUsage()
+    {
+        $quota = $this->getQuota();
+        if (is_array($quota)) {
+            $quota = $quota['STORAGE']['usage'];
+        }
+
+        return $quota;
+    }
 }
